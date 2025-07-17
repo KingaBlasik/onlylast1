3981897f5f6d1ab8c4ad9ba5c6dcdb1c61c99241
KingaBlasik <kingablasik@gmail.com>
2025-07-17 22:35:42 +0200

    Update offers.js
    Add 3 fake hotel offers

--- api/offers.js
+++ api/offers.js
@@ -1,1 +1,25 @@
-
+export default function handler(req, res) {
+  res.status(200).json([
+    {
+      id: 1,
+      name: "Hotel Sunset",
+      location: "Gdańsk",
+      price: "320 zł / noc",
+      image: "https://source.unsplash.com/featured/?hotel,1"
+    },
+    {
+      id: 2,
+      name: "Cozy Mountain Lodge",
+      location: "Zakopane",
+      price: "270 zł / noc",
+      image: "https://source.unsplash.com/featured/?hotel,2"
+    },
+    {
+      id: 3,
+      name: "City Lights Hotel",
+      location: "Warszawa",
+      price: "410 zł / noc",
+      image: "https://source.unsplash.com/featured/?hotel,3"
+    }
+  ]);
+}
